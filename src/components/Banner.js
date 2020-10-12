e5d15177733980beef0f4b3922d67be6fb88debc
Oscar Gross Jr <oscar.gross222@gmail.com>
2020-10-12 12:41:20 -0300

    Sort and Add Pages

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@ export default function Banner(props) {
     return (
         <section id="banner">
             <h1>{props.titulo}</h1>
-            <p>{props.mensagem}</p>
+            <p><i>{props.mensagem}</i></p>
         </section>
     )
 }
