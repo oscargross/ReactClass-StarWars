ce24c19dcef036916e016266f942afcefb9d4292
Oscar Gross Jr <oscar.gross222@gmail.com>
2020-10-13 23:27:27 -0300

    Task 2 - People - Concluded

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -9,36 +9,35 @@ export default function Login() {
 
     return (
         <>
-         <section class="home_banner_area blog_banner">
-                        <div class="banner_inner d-flex align-items-center">
-                            <div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
-                            <div class="container">
-                                <div class="blog_b_text text-center">
-                                    <Banner titulo={"Login" } mensagem="Luke, você descobrirá que muitas das verdades às quais nos prendemos dependem do nosso ponto de vista."/>
-                                    <a class="white_bg_btn" href="#">Veja Mais</a>
+         <section className="home_banner_area blog_banner">
+                        <div className="banner_inner d-flex align-items-center">
+                            <div className="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
+                            <div className="container">
+                                <div className="blog_b_text text-center">
+                                    <Banner titulo={"Login" } mensagem="Eu sou seu Pai!"/>
                                 </div>
                             </div>
                         </div>
                     </section>
-            <section class="furniture_area p_120">
-                <div class="container">
-                    <div class="main_title">
+            <section className="furniture_area p_120">
+                <div className="container">
+                    <div className="main_title">
                         <h2>Desenvolvedores</h2>
                         <p>Produzido por:</p>
                     </div>
-                    <div class="furniture_inner row">
-                        <div class="col-lg-6">
-                            <div class="furniture_item">
-                                <img class="img-fluid" src={require("../images/oscar.jpg")} alt="" />
-                                <h4 class="center">Oscar Gross Junior</h4>
-                                <p class="center">1118783<br/>1118783@imed.edu.br</p>
+                    <div className="furniture_inner row">
+                        <div className="col-lg-6">
+                            <div className="furniture_item">
+                                <img className="img-fluid" src={require("../images/oscar.jpg")} alt="" />
+                                <h4 className="center">Oscar Gross Junior</h4>
+                                <p className="center">1118783<br/>1118783@imed.edu.br</p>
                             </div>
                         </div>
-                        <div class="col-lg-6">
-                            <div class="furniture_item">
-                                <img class="img-fluid" src={require("../images/felipe.jpg")} alt="" />
-                                <h4 class="center">Felipe Daniel</h4>
-                                <p class="center">1118095<br/>1118095@imed.edu.br</p>
+                        <div className="col-lg-6">
+                            <div className="furniture_item">
+                                <img className="img-fluid" src={require("../images/felipe.jpg")} alt="" />
+                                <h4 className="center">Felipe Daniel</h4>
+                                <p className="center">1118095<br/>1118095@imed.edu.br</p>
                             </div>
                         </div>
                     </div>
--- src/pages/People.js
+++ src/pages/People.js
@@ -1,46 +1,62 @@
-import React from 'react'
+import React, { useLayoutEffect, useState } from 'react'
 //import { Link } from 'react-router-dom'
 import '../assets/css/style.css'
 import '../components/Banner'
 import Banner from '../components/Banner'
+const axios = require('axios');
+
+
 
 export default function People() {
+
+    const [people, setPeople] = useState([])
+
+    const getPeople = () => {
+        axios.get('https://swapi.dev/api/people/')
+            .then((response) => {
+                console.log(response.data);
+                setPeople(response.data.results)
+
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    }
+    useLayoutEffect(() => {
+        getPeople()
+    }, [])
 
 
     return (
         <>
-         <section class="home_banner_area blog_banner">
-                        <div class="banner_inner d-flex align-items-center">
-                            <div class="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
-                            <div class="container">
-                                <div class="blog_b_text text-center">
-                                    <Banner titulo={"Personagens" } mensagem="Luke, você descobrirá que muitas das verdades às quais nos prendemos dependem do nosso ponto de vista."/>
-                                    <a class="white_bg_btn" href="#">Veja Mais</a>
+            <section className="home_banner_area blog_banner">
+                <div className="banner_inner d-flex align-items-center">
+                    <div className="overlay bg-parallax" data-stellar-ratio="0.9" data-stellar-vertical-offset="0" data-background=""></div>
+                    <div className="container">
+                        <div className="blog_b_text text-center">
+                            <Banner titulo={"Personagens"} mensagem="Você faz aquilo que você acha que tem fazer." />
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section className="furniture_area p_120">
+                <div className="container">
+                    <div className="main_title">
+                        <h2>Personagens:</h2>
+                    </div>
+                    <div className="furniture_inner row">
+                        {people.map((item, key) =>
+                            <div className="col-lg-4" key={key} style={{ borderColor: "GrayText", borderStyle: "solid" }}>
+                                <div className="furniture_item" >
+                                    <h4 className="center">{item.name}</h4>
+                                    <p className="center">Altura: {item.height}</p>
+                                    <p className="center">Peso: {item.mass}</p>
+                                    <p className="center">Gênero: {item.gender}</p>
+                                    <p className="center">Cor do Cabelo: {item.hair_color}</p>
+
                                 </div>
                             </div>
-                        </div>
-                    </section>
-            <section class="furniture_area p_120">
-                <div class="container">
-                    <div class="main_title">
-                        <h2>Desenvolvedores</h2>
-                        <p>Produzido por:</p>
-                    </div>
-                    <div class="furniture_inner row">
-                        <div class="col-lg-6">
-                            <div class="furniture_item">
-                                <img class="img-fluid" src={require("../images/oscar.jpg")} alt="" />
-                                <h4 class="center">Oscar Gross Junior</h4>
-                                <p class="center">1118783<br/>1118783@imed.edu.br</p>
-                            </div>
-                        </div>
-                        <div class="col-lg-6">
-                            <div class="furniture_item">
-                                <img class="img-fluid" src={require("../images/felipe.jpg")} alt="" />
-                                <h4 class="center">Felipe Daniel</h4>
-                                <p class="center">1118095<br/>1118095@imed.edu.br</p>
-                            </div>
-                        </div>
+                        )}
                     </div>
                 </div>
             </section>
